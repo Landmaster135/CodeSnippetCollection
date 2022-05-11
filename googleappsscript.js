1258406b5bf198ff6340701b791925221a57842a
kinkinbeer135ml <52403447+landmaster135@users.noreply.github.com>
2022-05-11 11:16:19 -0700

    Update googleappsscript.js

--- googleappsscript.js
+++ googleappsscript.js
@@ -164,4 +164,31 @@ const res = UrlFetchApp.fetch(url, {
 })
 
 
+// Text by windows in GSS --------------------------------------------------------
+
+function doGetTestWithGssModelessDialog(e) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const html = HtmlService.createTemplateFromFile("index")
+    .evaluate()
+    .setTitle(SITE_TITLE)
+    .addMetaTag("viewport", "width=device-width, initial-scale=1");
+  ss.show(html);
+}
+
+function doGetTestWithGssSidebar(e) {
+  const html = HtmlService.createTemplateFromFile("index")
+    .evaluate()
+    .setTitle(SITE_TITLE)
+    .addMetaTag("viewport", "width=device-width, initial-scale=1");
+  SpreadsheetApp.getUi().showSidebar(html);
+}
+
+function doGetTestWithGssModalDialog(e) {
+  const html = HtmlService.createTemplateFromFile("index")
+    .evaluate()
+    .setTitle(SITE_TITLE)
+    .addMetaTag("viewport", "width=device-width, initial-scale=1");
+  SpreadsheetApp.getUi().showModalDialog(html, "Test dialog");
+}
+
 
